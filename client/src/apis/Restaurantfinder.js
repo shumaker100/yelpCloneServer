60c2b62c258d2bd817a62835c6a315f7e0f0bcce
Jason Shumaker <utahshu@gmail.com>
2021-02-26 14:35:35 -0700

    sdsdsds

--- client/src/apis/Restaurantfinder.js
+++ client/src/apis/Restaurantfinder.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `https://api/v1/restaurants/${process.env.API_ENDPOINT}`,
+  baseURL: `${process.env.API_ENDPOINT}api/v1/restaurants/`,
 });
