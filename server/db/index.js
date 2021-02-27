1a5152098e475b8ebeada3b7888ffd986ac59e88
Jason Shumaker <utahshu@gmail.com>
2021-02-27 09:57:02 -0700

    working

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,18 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  host: process.env.DATABASE_URL || "localhost",
-});
+const devConfig = {
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+};
+
+const proConfig = process.env.DATABASE_URL;
+const pool =
+  process.env.NODE_ENV == "production"
+    ? new Pool({ connectionString: proConfig })
+    : new Pool();
+
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
