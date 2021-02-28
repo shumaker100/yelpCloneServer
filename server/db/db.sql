418261d5d79c3de8024ad9c12bc2985f75d40390
Jason Shumaker <utahshu@gmail.com>
2021-02-27 18:22:18 -0700

    update

--- server/db/db.sql
+++ server/db/db.sql
@@ -1,20 +1,20 @@
 CREATE TABLE restaurants (
-    id BIGSERIAL INT NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    location VARCHAR(50) NOT NULL,
-    price_range INT NOT NULL check(price_range >= 1 and price_range <= 5)
-)
+    id bigint NOT NULL,
+    name character varying(50) NOT NULL,
+    location character varying(50) NOT NULL,
+    price_range integer NOT NULL,
+    CONSTRAINT restaurants_price_range_check CHECK (((price_range >= 1) AND (price_range <= 5)))
+);
 
 CREATE TABLE reviews (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
-    name VARCHAR(50) NOT NULL,
-    review TEXT NOT NULL,
-    rating INT NOT NULL check(
-        rating >= 1
-        and rating <= 5
-    )
+    id bigint NOT NULL,
+    restaurant_id bigint NOT NULL,
+    name character varying(50) NOT NULL,
+    review text NOT NULL,
+    rating integer NOT NULL,
+    CONSTRAINT reviews_rating_check CHECK (((rating >= 1) AND (rating <= 5)))
 );
+
 
 select *
 from restaurants
